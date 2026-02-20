086a0447bc6de77421634faa955cd339800b6765
Yugeun Song <yugeunsong2002@gmail.com>
2026-02-20 23:07:12 +0900

    feat: add snacks.nvim and enable hidden/ignored files in explorer

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+            ignored = true,
+            exclude = {},
+          },
+          files = {
+            hidden = true,
+            ignored = true,
+            exclude = {},
+          },
+          grep = {
+            hidden = true,
+            ignored = true,
+            exclude = {},
+          },
+        },
+      },
+    },
+  },
+}
