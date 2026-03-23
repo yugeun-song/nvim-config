7ba95c85246985224b5b8f31cf192c560ceebe0e
Yugeun Song <yugeunsong2002@gmail.com>
2026-03-23 23:48:04 +0900

    feat: add gruvbox theme and set it as the default

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,19 @@
 return {
   { "rktjmp/lush.nvim" },
-  {
-    "kartikp10/noctis.nvim",
-    lazy = false,
-    priority = 1000,
-  },
+  { "kartikp10/noctis.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "noctis",
+      colorscheme = "gruvbox-material",
     },
+  },
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_better_performance = 1
+    end,
   },
 }
