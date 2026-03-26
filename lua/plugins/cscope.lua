cdcb5dd523d7c3c2ca939eafdd65730e970cc6e0
Yugeun Song <yugeunsong2002@gmail.com>
2026-03-26 13:57:26 +0900

    perf(plugins): lazy-load cscope, tweak neovide, and config snacks picker/bigfile

--- lua/plugins/cscope.lua
+++ lua/plugins/cscope.lua
@@ -2,17 +2,17 @@ return {
   {
     "dhananjaylatkar/cscope_maps.nvim",
     dependencies = { "nvim-telescope/telescope.nvim" },
+    keys = {
+      { "<leader>cs", [[<cmd>exe "Cscope find s " . expand("<cword>")<cr>]], desc = "Cscope Find Symbol" },
+      { "<leader>cg", [[<cmd>exe "Cscope find g " . expand("<cword>")<cr>]], desc = "Cscope Find Definition" },
+      { "<leader>cc", [[<cmd>exe "Cscope find c " . expand("<cword>")<cr>]], desc = "Cscope Find Callees" },
+    },
     opts = {
       disable_navigation_mappings = false,
       use_telescope = true,
       cscope = {
         picker = "telescope",
       },
-    },
-    keys = {
-      { "<leader>cs", [[<cmd>exe "Cscope find s " . expand("<cword>")<cr>]], desc = "Cscope Find Symbol" },
-      { "<leader>cg", [[<cmd>exe "Cscope find g " . expand("<cword>")<cr>]], desc = "Cscope Find Definition" },
-      { "<leader>cc", [[<cmd>exe "Cscope find c " . expand("<cword>")<cr>]], desc = "Cscope Find Callees" },
     },
   },
 }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,22 +2,23 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      bigfile = {
+        enabled = true,
+        size = 5 * 1024 * 1024,
+      },
       picker = {
         sources = {
           explorer = {
             hidden = true,
             ignored = true,
-            exclude = {},
           },
           files = {
             hidden = true,
             ignored = true,
-            exclude = {},
           },
           grep = {
             hidden = true,
             ignored = true,
-            exclude = {},
           },
         },
       },
