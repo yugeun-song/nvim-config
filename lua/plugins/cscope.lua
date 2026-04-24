90366cd6a873058861ead120fa2da2905defadc8
Yugeun Song <yugeunsong2002@gmail.com>
2026-04-25 01:56:47 +0900

    feat(plugins): move cscope to leader-i and disable conflicting defaults

--- lua/plugins/cscope.lua
+++ lua/plugins/cscope.lua
@@ -2,16 +2,26 @@ return {
   {
     "dhananjaylatkar/cscope_maps.nvim",
     dependencies = { "nvim-telescope/telescope.nvim" },
+    event = { "BufReadPre", "BufNewFile" },
     keys = {
-      { "<leader>cs", [[<cmd>exe "Cscope find s " . expand("<cword>")<cr>]], desc = "Cscope Find Symbol" },
-      { "<leader>cg", [[<cmd>exe "Cscope find g " . expand("<cword>")<cr>]], desc = "Cscope Find Definition" },
-      { "<leader>cc", [[<cmd>exe "Cscope find c " . expand("<cword>")<cr>]], desc = "Cscope Find Callees" },
+      { "<leader>is", [[<cmd>exe "Cscope find s " . expand("<cword>")<cr>]], desc = "Cscope: Find Symbol" },
+      { "<leader>ig", [[<cmd>exe "Cscope find g " . expand("<cword>")<cr>]], desc = "Cscope: Find Global Def" },
+      { "<leader>ic", [[<cmd>exe "Cscope find c " . expand("<cword>")<cr>]], desc = "Cscope: Find Callers" },
+      { "<leader>id", [[<cmd>exe "Cscope find d " . expand("<cword>")<cr>]], desc = "Cscope: Find Callees" },
+      { "<leader>it", [[<cmd>exe "Cscope find t " . expand("<cword>")<cr>]], desc = "Cscope: Find Text" },
+      { "<leader>ie", [[<cmd>exe "Cscope find e " . expand("<cword>")<cr>]], desc = "Cscope: Find Egrep" },
+      { "<leader>if", [[<cmd>exe "Cscope find f " . expand("<cfile>")<cr>]], desc = "Cscope: Find File" },
+      { "<leader>ii", [[<cmd>exe "Cscope find i " . expand("<cfile>")<cr>]], desc = "Cscope: Find Includes" },
+      { "<leader>ia", [[<cmd>exe "Cscope find a " . expand("<cword>")<cr>]], desc = "Cscope: Find Assignments" },
     },
     opts = {
-      disable_navigation_mappings = false,
-      use_telescope = true,
+      disable_maps = true,
       cscope = {
+        db_file = {},
         picker = "telescope",
+        tag = {
+          keymap = false,
+        },
       },
     },
   },
