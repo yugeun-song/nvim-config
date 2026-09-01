77e83bb7dd7c8932020fc47c195c97f4f87dfddf
Yugeun Song <yugeunsong2002@gmail.com>
2026-09-02 03:24:55 +0900

    chore: drop a stray emoji from the example spec

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -159,7 +159,7 @@ return {
     opts = function(_, opts)
       table.insert(opts.sections.lualine_x, {
         function()
-          return "😄"
+          return ""
         end,
       })
     end,
